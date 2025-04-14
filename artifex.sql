fb3036b94431eb2c465e1ad6a73a4aa4690218a9
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2025-04-14 12:32:25 +0200

    Update artifex.sql

--- artifex.sql
+++ artifex.sql
@@ -1,1 +1,13 @@
 -- Progetto artifex database
+create database artifex;
+
+create table amministratori(
+	email varchar(100) primary key,
+	password varchar(100)
+);
+
+
+create table visitatori(
+
+
+);
