57b26976df98f5c106b9c56d243338c6df06b09b
Luca Buttini <121725497+LucaButtini@users.noreply.github.com>
2025-04-15 08:52:53 +0200

    Update artifex.sql

--- artifex.sql
+++ artifex.sql
@@ -1,5 +1,7 @@
 -- Progetto artifex database
-create database artifex;
+create database artifex_buttini;
+
+use artifex_buttini;
 
 create table amministratori(
 	email varchar(100) primary key,
@@ -8,6 +10,46 @@ create table amministratori(
 
 
 create table visitatori(
-
-
+	codice_fiscale varchar(50) primary key,
+	nome varchar(100),
+	email varchar(100),
+	nazionalita varchar(100),
+	telefono varchar(50),
+	lingua_base varchar(50)
 );
+
+
+create table visite(
+ 	titolo varchar(100) primary key,
+ 	durata_media time,
+ 	luogo varchar(100)
+);
+
+
+create table eventi(
+	id_evento int primary key auto_increment,
+	prezzo double,
+	min_persone int,
+	max_persone int
+);
+
+create table prenotazioni(
+visitatore varchar(50),
+id_evento int,
+primary key(visitatore, id_evento),
+foreign key (visitatore) references eventi(codice_fiscale)
+foreign key (id_evento) references eventi(id_evento)
+);
+
+
+
+create table eventi_visite(
+	titolo_visita varchar(100),
+	id_evento int,
+	data_visita datetime,
+	primary key(titolo_visita, id_evento),
+	foreign key (titolo_visita) references visite(titolo),
+	foreign key (id_evento) references eventi(id_evento)
+);
+
+
