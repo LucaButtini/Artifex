c7c240e15ed97d8d0ae3de8c88bf759fc24765d5
LucaButtini <luca.buttini@iisviolamarchesini.edu.it>
2025-05-10 15:07:09 +0200

    non visualizza prenotazioni su profile

--- App/View/header.php
+++ App/View/header.php
@@ -31,6 +31,7 @@ $username = $visitorSession['nome']
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= $href ?>">
+    <link rel="icon" type="image/x-icon" href="/Artifex/Public/Immagini/bank.ico">
     <title><?= $title ?? 'Artifex' ?></title>
 </head>
 <body class="d-flex flex-column min-vh-100">
